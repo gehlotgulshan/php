5fefb6af4b581f7c25b3eab02f3587e0bff3fb52
gehlot Gulshan <gulshangehlot@gmail.com>
2023-12-26 18:02:18 +0530

    first commit

--- php/index.php
+++ php/index.php
@@ -8,15 +8,19 @@
 <body>
     <?php 
     $name = "Gulshan Gehlot";
+    $place1 = "Jaipur";
     $age = 22 ;
 
 
-    $fname = "Ajay";
-    $lname = "Singh";
+    $fname = "Ajay Singh";
+    $place2 = "Kota";
 
+    $fname3 = "Vijay Kapoor";
+    $place3 = "Ajmer";
     
-    echo "My Name is $name...! <br> Age is $age<br>";
-    echo "My Name is $fname $lname...! <br> Age is $age<br>";
+    echo "My Name is $name...! <br> Age is $age From $place1<br>";
+    echo "My Name is $fname...! <br> Age is $age From $place2<br>";
+    echo "My Name is $fname3...! <br> Age is $age From $place3<br>";
   
     ?>
 </body>
