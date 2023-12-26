c705e3b1d1ee4c13f9dda5d3846bf45adfe157ab
gehlot Gulshan <gulshangehlot@gmail.com>
2023-12-26 17:31:22 +0530

    first commit

---
+++ php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "Title" ; ?></title>
+</head>
+<body>
+    <?php 
+    $name = "Gulshan Gehlot";
+    $age = 22 ;
+
+    
+    echo "My Name is $name...! <br> Age is $age<br>";
+    ?>
+</body>
+</html>
